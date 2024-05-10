06204064b8936468bb075b066dd6ba59486984f2
azad prasad <imazadkumar88044@gmail.com>
2024-05-10 14:10:06 +0530

    Update Combination Sum II algorithm

---
+++ DSA-LeetCode/Recursion/40.-Combination-Sum-II.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+class Solution {
+public:
+void  solvecombinationSum( vector<int>& candidates, int target,vector<int>&v,vector<vector<int>>&ans,int start)
+{ 
+    sort(candidates.begin(),candidates.end());
+    
+    if(target==0)
+    {
+        ans.push_back(v);
+        return;
+    }
+    if(target<0)
+    {
+        return;
+    }
+    for(int i=start;i<candidates.size();i++)
+    {
+        if(i>start&& candidates[i]==candidates[i-1])
+        {
+            continue;
+        }
+        v.push_back(candidates[i]);
+        solvecombinationSum(candidates,target-candidates[i],v,ans,i+1);
+        v.pop_back();
+    }
+}
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>>ans;
+        vector<int>v;
+        
+        solvecombinationSum(candidates,  target,v,ans,0);
+        return ans;
+    }
+};
