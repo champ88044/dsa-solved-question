557dd59fcd743f3b34930396540daacf7127011f
azad prasad <imazadkumar88044@gmail.com>
2024-05-12 22:16:14 +0530

    13.-Roman-to-Integer.cpp

--- DSA-LeetCode/Arrays/13.-Roman-to-Integer.cpp
+++ DSA-LeetCode/Arrays/13.-Roman-to-Integer.cpp
@@ -1,63 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-class Solution {
-public:
-int value(char c)
+class Solution
 {
-     switch(c)
+public:
+    int romanToInt(string s)
+    {
+        int sum = 0;
+        unordered_map<char, int> m;
+        m['I'] = 1;
+        m['V'] = 5;
+        m['X'] = 10;
+        m['L'] = 50;
+        m['C'] = 100;
+        m['D'] = 500;
+        m['M'] = 1000;
+        for (int i = 0; i < s.length(); i++)
         {
-            case 'M':
-            return 1000;
-            break;
-
-             case 'D':
-            return 500;
-            break;
-
-            case 'C':
-            return 100;
-            break;
-
-             case 'L':
-            return 50;
-            break;
-
-             case 'X':
-            return 10;
-            break;
-
-             case 'V':
-            return 5;
-            break;
-
-            case 'I':
-            return 1;
-            break;
-            default :
-            return -1;
-        }
-}
-    int romanToInt(string s) {
-            int sum=0;
-             unordered_map<char,int>m;
-        m['I']=1;
-        m['V']=5;
-        m['X']=10;
-        m['L']=50;
-        m['C']=100;
-        m['D']=500;
-        m['M']=1000;
-            for(int i=0;i<s.length();i++)
+            if (m[s[i]] < m[s[i + 1]])
             {
-                if(m[s[i]]<m[s[i+1]])
-                {
-                   sum-=m[s[i]];
-                }
-                else
-                {
-                    sum+=m[s[i]];
-                }
+                sum -= m[s[i]];
             }
-            return (sum);
+            else
+            {
+                sum += m[s[i]];
+            }
+        }
+        return (sum);
     }
 };
