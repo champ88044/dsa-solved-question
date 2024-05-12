06645b09fcdc1d047094af371c6b654c0ab00357
azad prasad <imazadkumar88044@gmail.com>
2024-05-12 22:10:42 +0530

    Update Roman-to-Integer and Largest-Local-Values-in-a-Matrix

--- DSA-LeetCode/Arrays/13.-Roman-to-Integer.cpp
+++ DSA-LeetCode/Arrays/13.-Roman-to-Integer.cpp
@@ -1,34 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
+class Solution {
+public:
+int value(char c)
+{
+     switch(c)
+        {
+            case 'M':
+            return 1000;
+            break;
 
-int value(char r) {
-    if (r == 'I') return 1;
-    if (r == 'V') return 5;
-    if (r == 'X') return 10;
-    if (r == 'L') return 50;
-    if (r == 'C') return 100;
-    if (r == 'D') return 500;
-    if (r == 'M') return 1000;
-    return -1;
-}
+             case 'D':
+            return 500;
+            break;
 
-void  romanToDecimal(string& str) {
-    int res = 0;
-    for (int i = 0; i < str.length(); i++) {
-       if(value(str[i]<value(str[i+1])))
-       {
-        res-=value(str[i]);
-        cout<<"res="<<res<<"  -> "<<endl;
-       }
-        else {
-            res +=value(str[i]);
+            case 'C':
+            return 100;
+            break;
+
+             case 'L':
+            return 50;
+            break;
+
+             case 'X':
+            return 10;
+            break;
+
+             case 'V':
+            return 5;
+            break;
+
+            case 'I':
+            return 1;
+            break;
+            default :
+            return -1;
         }
+}
+    int romanToInt(string s) {
+            int sum=0;
+             unordered_map<char,int>m;
+        m['I']=1;
+        m['V']=5;
+        m['X']=10;
+        m['L']=50;
+        m['C']=100;
+        m['D']=500;
+        m['M']=1000;
+            for(int i=0;i<s.length();i++)
+            {
+                if(m[s[i]]<m[s[i+1]])
+                {
+                   sum-=m[s[i]];
+                }
+                else
+                {
+                    sum+=m[s[i]];
+                }
+            }
+            return (sum);
     }
-   // return res;
-}
-
-int main() {
-    string str = "LVIII";
-     romanToDecimal(str);
-    return 0;
-}
+};
