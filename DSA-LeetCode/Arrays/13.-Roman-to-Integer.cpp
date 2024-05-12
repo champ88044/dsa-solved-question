7c6a8ed26af4a51f14fcd0b50cc93e37a0100e74
Azad Prasad <137818756+champ88044@users.noreply.github.com>
2024-05-12 22:27:05 +0530

    Update 13.-Roman-to-Integer.cpp

--- DSA-LeetCode/Arrays/13.-Roman-to-Integer.cpp
+++ DSA-LeetCode/Arrays/13.-Roman-to-Integer.cpp
@@ -2,41 +2,7 @@
 using namespace std;
 class Solution {
 public:
-int value(char c)
-{
-     switch(c)
-        {
-            case 'M':
-            return 1000;
-            break;
 
-             case 'D':
-            return 500;
-            break;
-
-            case 'C':
-            return 100;
-            break;
-
-             case 'L':
-            return 50;
-            break;
-
-             case 'X':
-            return 10;
-            break;
-
-             case 'V':
-            return 5;
-            break;
-
-            case 'I':
-            return 1;
-            break;
-            default :
-            return -1;
-        }
-}
     int romanToInt(string s) {
             int sum=0;
              unordered_map<char,int>m;
