eb7e310c9733bb45a9698cb96e005958d314c2c7
azad prasad <imazadkumar88044@gmail.com>
2024-06-13 22:25:43 +0530

    DSA-LeetCode/Arrays/Minimum-Number-of-Moves-to-Seat-Everyone.cpp

---
+++ DSA-LeetCode/Arrays/Minimum-Number-of-Moves-to-Seat-Everyone.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int minMovesToSeat(vector<int> &seats, vector<int> &students)
+    {
+        sort(seats.begin(), seats.end());
+        sort(students.begin(), students.end());
+        int sum = 0;
+        for (int i = 0; i < seats.size(); i++)
+        {
+            sum += abs(seats[i] - students[i]);
+        }
+        return sum;
+    }
+};
