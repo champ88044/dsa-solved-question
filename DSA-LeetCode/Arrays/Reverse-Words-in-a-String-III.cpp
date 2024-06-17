958124b683019ff489eedbcb47424d831f1d6b9f
azad prasad <imazadkumar88044@gmail.com>
2024-06-17 22:42:43 +0530

    DSA-LeetCode/Arrays/Reverse-Words-in-a-String-III.cpp

---
+++ DSA-LeetCode/Arrays/Reverse-Words-in-a-String-III.cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+public:
+    string reverseWords(string s)
+    {
+        //   //  tc O(n)
+        //      int n=s.length();
+        //     for(int i=0;i<n;i++)
+        //     {
+        //        if(s[i]!=' ')
+        //        {
+        //         int j=i;
+        //         while(j<n&&s[j]!=' ')
+        //         {
+        //             j++;
+        //         }
+        //         reverse(begin(s)+i,begin(s)+j);
+        //         i=j;
+        //        }
+        //     }
+        //     return s;
+
+        // approach 2
+        stringstream ss(s);
+        string token = "";
+        string result = "";
+        while (ss >> token)
+        {
+            reverse(begin(token), end(token));
+            result += token + " ";
+        }
+        return result.substr(0, result.length() - 1);
+    }
+};
