eadb1cbf9250ca0448c879e69de3ca91ba271655
azad prasad <imazadkumar88044@gmail.com>
2024-07-06 11:55:08 +0530

    Your commit message here

---
+++ DSA-LeetCode/Math-Que/2582-Pass-The_Pillow.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+// time complexity O(N)
+class Solution
+{
+public:
+    int passThePillow(int n, int time)
+    {
+        int i = 1;
+        while (time > 0)
+        {
+            while (i < n && time > 0)
+            {
+                i++, time--;
+            }
+            while (i > 1 && time > 0)
+            {
+                i--, time--;
+            }
+        }
+        return i;
+    }
+};
