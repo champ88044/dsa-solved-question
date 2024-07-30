1e62d0392d3b6561c158e822e21e6f5f268d248e
azad prasad <imazadkumar88044@gmail.com>
2024-07-30 23:07:16 +0530

    DSA-LeetCode/Stack/1653.Minimum-Deletions-to-Make-String-Balanced.cpp

---
+++ DSA-LeetCode/Stack/1653.Minimum-Deletions-to-Make-String-Balanced.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int minimumDeletions(string s)
+    {
+        stack<char> st;
+        int count = 0;
+        for (int i = 0; i < s.size(); i++)
+        {
+            char ch = s[i];
+
+            if (!st.empty() && ch == 'a' && st.top() == 'b')
+            {
+                count++;
+                st.pop();
+            }
+            else
+            {
+                st.push(ch);
+            }
+        }
+        return count;
+    }
+};
