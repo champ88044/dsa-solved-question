86c366d76ff38ff1d86fb90ba7f5f93c5420153b
azad prasad <imazadkumar88044@gmail.com>
2024-08-01 11:11:29 +0530

    Your commit message here

---
+++ DSA-LeetCode/LeetCode-75/1768.-Merge-Strings-Alternately.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    string mergeAlternately(string word1, string word2)
+    {
+        string ans = "";
+        int n = word1.length(), m = word2.length();
+        for (int i = 0; i < max(n, m); i++)
+        {
+            if (i < n)
+                ans += word1[i];
+            if (i < m)
+                ans += word2[i];
+        }
+        return ans;
+    }
+};
