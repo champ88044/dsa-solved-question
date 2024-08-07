4ed2f417301cd26e843805312dbaa51ec9f468cd
azad prasad <imazadkumar88044@gmail.com>
2024-08-07 23:34:28 +0530

    DSA-LeetCode/LeetCode-75/1071.-Greatest-Common-Divisor-of-Strings.cpp

---
+++ DSA-LeetCode/LeetCode-75/1071.-Greatest-Common-Divisor-of-Strings.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    string gcdOfStrings(string s1, string s2)
+    {
+        int n1 = s1.size(), n2 = s2.size();
+        if (s1 + s2 == s2 + s1)
+            return s1.substr(0, __gcd(n1, n2));
+        else
+            return "";
+    }
+};
