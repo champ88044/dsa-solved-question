8ff86b8d070d18d95a647873ce69ae1eb7c9c09c
azad prasad <imazadkumar88044@gmail.com>
2024-08-07 23:59:52 +0530

    DSA-LeetCode/LeetCode-75/1431.Kids-With-the-Greatest-Number-of-Candies.cpp

---
+++ DSA-LeetCode/LeetCode-75/1431.Kids-With-the-Greatest-Number-of-Candies.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    vector<bool> kidsWithCandies(vector<int> &candies, int extraCandies)
+    {
+        int n = candies.size();
+        vector<bool> ans;
+        int maxi = 0;
+        for (auto i : candies)
+        {
+            maxi = max(i, maxi);
+        }
+        for (int i = 0; i < n; i++)
+        {
+            if (candies[i] + extraCandies >= maxi)
+            {
+                ans.push_back(true);
+            }
+            else
+            {
+                ans.push_back(false);
+            }
+        }
+        return ans;
+    }
+};
